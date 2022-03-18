e0635f386d2f42083933cedcda48ccf8726a561a
Sunil4517 <sunilkumar.bisai@gmail.com>
2022-03-18 07:26:03 +0530

    added main.tf

---
+++ main.tf
@@ -0,0 +1,17 @@
+data "aws_vpc" "default" {
+  default = true
+}
+
+data "aws_subnet_ids" "all" {
+  vpc_id = data.aws_vpc.default.id
+}
+
+data "aws_subnets" "default" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
+}
+data "aws_availability_zones" "available" {
+  state = "available"
+}
